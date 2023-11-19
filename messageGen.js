5c196ad2d3671db301605c427766b5efe9807983
Miguel Gomes <miguelgomesdzn@gmail.com>
2023-11-19 09:18:35 +0000

    Add generative  fuctions and is working

--- messageGen.js
+++ messageGen.js
@@ -1,1 +1,23 @@
-/*   INICIO */
+/*   MESSAGE GENERATOR */
+
+let mgPart1 =["I'm graitfull", "For the future"];
+let mgPart2 =["want to be", "help people"];
+let mgPart3 =["great", "hope", "happy", "friend"];
+
+
+
+const randomParts= arr => {
+
+   let radNum = Math.floor(Math.random()*arr.length);
+   return arr[radNum];
+
+} // //
+
+
+const generateMessage=(part1, part2, part3  )=>{
+    return `${randomParts(part1)} ${randomParts(part2)} ${randomParts(part3)} `;
+} // //
+
+
+//console.log( randomParts(mgPart1) );
+console.log( generateMessage(mgPart1,mgPart2, mgPart3 ) );
