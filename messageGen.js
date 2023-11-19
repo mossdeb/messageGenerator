592299be25e719de0f52aedb14023f34427e0f2f
Miguel Gomes <miguelgomesdzn@gmail.com>
2023-11-19 09:33:33 +0000

    add more functions

--- messageGen.js
+++ messageGen.js
@@ -1,10 +1,15 @@
 /*   MESSAGE GENERATOR */
 
+/*Motivational Speach /vs Crazy talk */
+
 let mgPart1 =["I'm graitfull", "For the future"];
 let mgPart2 =["want to be", "help people"];
 let mgPart3 =["great", "hope", "happy", "friend"];
-
-
+///////////
+let czPart1 =["Hey Dude", "Please cleen your nose"];
+let czPart2 =["Some dome guys", "What do you want"];
+let czPart3 =["Bubbles", "pencil", "food", "Burger"];
+//////////
 
 const randomParts= arr => {
 
@@ -19,5 +24,19 @@ const generateMessage=(part1, part2, part3  )=>{
 } // //
 
 
-//console.log( randomParts(mgPart1) );
-console.log( generateMessage(mgPart1,mgPart2, mgPart3 ) );
+const messageGen=(number)=>{
+
+    if(number === 1){
+        console.log( generateMessage(mgPart1,mgPart2, mgPart3 ) );
+
+    }else if(number===2){
+
+        console.log( generateMessage(czPart1,czPart2, czPart3 ) );
+    }
+
+
+}
+
+
+
+messageGen(2);
